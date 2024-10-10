534b80c43aafc20d80d039cfd068e00e985e30b3
Ayanaanonaeem <ayanaano5@gmail.com>
2024-10-10 08:29:32 +0500

    file create

---
+++ app.js
@@ -0,0 +1,190 @@
+var one=document.getElementById("one")
+var two=document.getElementById("two")
+var three=document.getElementById("three")
+var four=document.getElementById("four")
+var five=document.getElementById("five")
+var six=document.getElementById("six")
+var seven=document.getElementById("seven")
+var eight=document.getElementById("eight")
+var nine=document.getElementById("nine")
+var ten=document.getElementById("ten")
+var eleven=document.getElementById("eleven")
+var main=document.getElementById("main")
+var main_1=document.getElementById("main_1")
+var sub=document.getElementById("sub")
+// var navbar=document.getElementById("navbar")
+
+
+one.addEventListener('mouseenter',function(){
+    // main.style.transition="all ease-in 0.5s";
+    // main.style.border="3px solid green"
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+   
+    
+
+})
+one.addEventListener('mouseout',function(){
+     main.style.height=""
+      sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+     main_1.style.filter="";
+     sub.style.visibility="hidden"
+    
+     
+})
+two.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+two.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+three.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+three.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+four.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+four.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+five.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+five.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+six.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+    
+
+})
+six.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+seven.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+seven.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+eight.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+eight.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+nine.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+nine.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+ten.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+
+})
+ten.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
+eleven.addEventListener('mouseenter',function(){
+    main_1.style.filter="blur(10px)";
+    sub.style.visibility="visible"
+    sub.style.transition="all ease-in 0.5s"
+    sub.style.height="350px"
+    
+    
+
+})
+eleven.addEventListener('mouseout',function(){
+    main.style.height=""
+    main_1.style.filter="";
+    sub.style.visibility="hidden"
+     sub.style.transition="all ease-in 0.5s"
+       sub.style.height="0px"
+})
